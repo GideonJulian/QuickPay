056911649977a8a6e2ed5d8f9835b89877beff3d
Gideon Julian <gideonchinonso77@gmail.com>
2025-11-07 15:17:50 +0100

    updated

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,10 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-
+import { useAuth } from '../../context/AuthContext';
 const index = () => {
+  const {user} = useAuth();
   return (
-    <View style={{ backgroundColor: "#111111", flex: 1 }}>
-      <Text>index</Text>
+    <View style={{ backgroundColor: "#111111", flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{color: 'white'}}>
+        {user ? `Welcome, ${user.name}!` : 'Welcome to the QuickPay App!'}
+      </Text>
     </View>
   )
 }
