0f1394ae32f6f7c5e2c4d55a9127de90dd14cecd
Gideon Julian <gideonchinonso77@gmail.com>
2025-11-10 15:17:51 +0100

    updated

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,5 +1,7 @@
 import { Tabs } from "expo-router";
-
+import Feather from "@expo/vector-icons/Feather";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 export default function Layout() {
   return (
     <Tabs
@@ -13,13 +15,51 @@ export default function Layout() {
           borderTopColor: "transparent",
           height: 67,
         },
-        tabBarActiveTintColor: ''
+        tabBarActiveTintColor: "#4f46e5",
+        tabBarInactiveTintColor: "#a0a0a5",
       }}
     >
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="profile" options={{ title: "Profile" }} />
-      <Tabs.Screen name="history" options={{ title: "history" }} />
-      <Tabs.Screen name="payment" options={{ title: "Payment" }} />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="home" size={22} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="history"
+        options={{
+          title: "history",
+          tabBarIcon: ({ size, color }) => (
+            <MaterialCommunityIcons name="history" size={22} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="payment"
+        options={{
+          title: "Payment",
+          tabBarIcon: ({ size, color }) => (
+            <MaterialCommunityIcons
+              name="wallet-outline"
+              size={22}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+            tabBarIcon: ({ size, color }) => (
+           <FontAwesome5 name="user" size={22} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,18 +1,24 @@
 import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { useAuth } from "../../context/AuthContext";
 import { Image } from "react-native";
 import userImage from "../../assets/images/user.png";
+import Feather from "@expo/vector-icons/Feather";
+import { TouchableOpacity } from "react-native";
 const index = () => {
   const { user } = useAuth();
+  const [eyeShow, setEyeShow] = useState(true);
+  const handleHideShow = () => {
+    setEyeShow(!eyeShow);
+  };
   return (
-    <View style={{ backgroundColor: "#111111", flex: 1, paddingVertical: 45, }}>
+    <View style={{ backgroundColor: "#111111", flex: 1, paddingVertical: 45 }}>
       <View
         style={{
           flexDirection: "row",
           justifyContent: "space-between",
           paddingHorizontal: 10,
-          alignItems: 'center' 
+          alignItems: "center",
         }}
       >
         <Text style={{ color: "white", fontWeight: "bold", fontSize: 20 }}>
@@ -39,7 +45,20 @@ const index = () => {
         </View>
       </View>
       <View style={styles.balanceContainer}>
+        <Text style={{ color: "#a0a0a0" }}>Your Balance</Text>
+        <View style={{ flexDirection: "row", gap: 5, alignItems: "center" }}>
+          <Text style={{ color: "white", fontWeight: "bold", fontSize: 32 }}>
+            $1,243.52
+          </Text>
 
+          <TouchableOpacity onPress={handleHideShow}>
+            {eyeShow ? (
+              <Feather name="eye" size={20} color="#a0a0a0" />
+            ) : (
+              <Feather name="eye-off" size={20} color="#a0a0a0" />
+            )}
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
@@ -48,6 +67,7 @@ const index = () => {
 export default index;
 const styles = StyleSheet.create({
   balanceContainer: {
-
-  }
-})
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
