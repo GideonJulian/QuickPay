ae49fdc234f0110fde91cd5da072dda39c553617
Gideon Julian <gideonchinonso77@gmail.com>
2025-11-11 15:34:27 +0100

    updated

--- components/HistoryItem.jsx
+++ components/HistoryItem.jsx
@@ -1,12 +1,22 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
 
-const HistoryItem = () => {
+const HistoryItem = ({ id, icon, title, price, date }) => {
   return (
-    <View>
-      <Text>HistoryItem</Text>
+    <View style={Styles.item}>
+      <View key={id}> </View>
     </View>
-  )
-}
+  );
+};
 
-export default HistoryItem
+export default HistoryItem;
+
+const Styles = StyleSheet.create({
+  item: {
+    width: "100%",
+    padding: 10,
+    backgroundColor: "#2a2a2a",
+    borderRadius: 8,
+    marginBottom: 10,
+  },
+});
--- components/HomeHistory.jsx
+++ components/HomeHistory.jsx
@@ -1,8 +1,18 @@
 import { View, Text, Pressable } from "react-native";
 import React from "react";
 import { Link } from "expo-router";
+import HistoryItem from "./HistoryItem";
 
 const HomeHistory = () => {
+  const historyData = [
+    {
+      id: 1,
+      icon: "💸",
+      title: "Sent to John",
+      price: "-$50.00",
+      date: "2024-06-10",
+    },
+  ];
   return (
     <View>
       <View
@@ -17,9 +27,16 @@ const HomeHistory = () => {
         </Text>
         <Pressable>
           <Link href={""}>
-            <Text style={{color: '#1f75ff', fontWeight: '500', fontSize:14}}>View All</Text>
+            <Text style={{ color: "#1f75ff", fontWeight: "500", fontSize: 14 }}>
+              View All
+            </Text>
           </Link>
         </Pressable>
+      </View>
+      <View style={{ marginTop: 10, gap: 10 }}>
+        {historyData.map((item) => (
+        <HistoryItem />
+        ))}
       </View>
     </View>
   );
